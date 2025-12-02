33839c22190a49eb611b5336aefb5881e64c90a9
Arthur Cesar <arthurcesar2002@gmail.com>
2025-12-02 15:12:59 -0300

    atualização

--- Scahrszchild_simulacao/main.py
+++ Scahrszchild_simulacao/main.py
@@ -5,18 +5,23 @@ import physics as phy
 import visualizacao as vis
 
 vis.configurar_cena()
-h = np.array ([-10*cfg.M, 5, 0, 1.0 , 0, 0])
+h = np.array ([-20*cfg.M, 5.19, 0, 1.0 , 0, 0])
 foton = sphere(pos = vector(h[0], h[1], h[2]),make_trail=True, radius = 0.1, color = color.yellow)
 
 #animação do fóton
-dt = 0.000005
+dt = 0.0005
 passo_por_frame = 1000
+capturado = False
 while True:
-    rate(200)
-    for i in range(passo_por_frame):
-        derivadas = phy.hamiltoniana_sistema(0, h)
-        h = h + derivadas * dt
-    foton.pos = vector(h[0], h[1], h[2])
+    rate(100)
+    if not capturado:
+        for i in range(passo_por_frame):
+            h = phy.rk4(0, h, dt)
+            distancia = np.linalg.norm(h[:3])
+            if distancia < cfg.r_s: #dentro do horizonte 
+                #foton cai na singularidade
+                capturado = True
+        foton.pos = vector(h[0], h[1], h[2])
     
 
 
